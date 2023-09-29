00ef621373981d276987a1487a7dfb5dd2d3b4f2
Kaustav-Bhattacharya <108495055+Kaustav-Bhattacharya@users.noreply.github.com>
2023-09-30 01:22:32 +0530

    saved favorite items to localstorage (#2)

--- src/pages/Home/types.tsx
+++ src/pages/Home/types.tsx
@@ -1,18 +1,14 @@
 interface PackageListProps {
-    list: PackageItem[];
-    update: React.Dispatch<React.SetStateAction<PackageItem[]>>;
-  }
+  list: PackageItem[];
+  update: React.Dispatch<React.SetStateAction<PackageItem[]>>;
+}
 
+interface PackageItem {
+  _id: string;
+  name: string;
+  description: string;
+  npmLink?: string;
+  reason: string;
+}
 
-  interface PackageItem {
-    name: string;
-    description:string,
-    npmLink?:string,
-    reason:string,
-  }
-
-
-  export type {
-    PackageItem,
-    PackageListProps
-  }
+export type { PackageItem, PackageListProps };
