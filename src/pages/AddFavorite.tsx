7474bd9ac0f9524f6f28a99d88c1b395841aa8b6
Kaustav-Bhattacharya <bhattacharyar64@gmail.com>
2023-09-30 12:05:51 +0530

    function name changed

--- src/pages/AddFavorite.tsx
+++ src/pages/AddFavorite.tsx
@@ -43,7 +43,7 @@ const AddFavorite: React.FC = () => {
     if (!debouncing && debouncedValue) fetchNpmPackages();
   }, [debouncedValue]);
 
-  const onCancel = () => {
+  const navigateHome = () => {
     navigate("/");
   };
 
@@ -87,73 +87,77 @@ const AddFavorite: React.FC = () => {
   };
 
   return (
-    <div className="container mx-auto mt-8 p-4">
-      <h2 className="text-left">
-        <strong>Add Fav. Package</strong>
-      </h2>
-      <div className="flex flex-row gap-3 mt-6">
-        <input
-          type="text"
-          placeholder="Search Package..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          className="w-full p-2 border rounded bg-slate-200"
-        />
-        <PrimaryButton
-          disabled={debouncing || !debouncedValue}
-          onClick={fetchNpmPackages}
-        >
-          Search
-        </PrimaryButton>
+    <div>
+      <div className="text-right">
+        <DangerButton onClick={navigateHome}>X</DangerButton>
       </div>
-      {loading && <Loader />}
-      {searchResults.length > 0 ? (
-        <div className="mt-10">
-          <h3 className="text-lg font-semibold text-left">Search Results:</h3>
-          <div className="max-h-40 overflow-y-auto mt-2 no-scrollbar">
-            {searchResults.map((pkg: any) => (
-              <div
-                key={pkg.package.name}
-                className={`text-left package-item p-2 cursor-pointer hover:bg-gray-300 ${
-                  pkg === selectedPackage ? "bg-blue-200" : ""
-                }`}
-                onClick={() => setSelectedPackage(pkg)}
-              >
-                {pkg.package.name}
-              </div>
-            ))}
-          </div>
-          {searchResults.length > 4 && (
-            <p className="m-0 p-0 text-center">
-              <strong>. . .</strong>
-            </p>
-          )}
+      <div className="container mx-auto mt-8 p-4">
+        <h2 className="text-left">
+          <strong>Add Fav. Package</strong>
+        </h2>
+        <div className="flex flex-row gap-3 mt-6">
+          <input
+            type="text"
+            placeholder="Search Package..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="w-full p-2 border rounded bg-slate-200"
+          />
+          <PrimaryButton
+            disabled={debouncing || !debouncedValue}
+            onClick={fetchNpmPackages}
+          >
+            Search
+          </PrimaryButton>
         </div>
-      ) : (
-        debouncedValue &&
-        !loading && (
-          <div className="mt-10 text-center transform scale-100 translate-y-0 transition-opacity ease-in duration-500 bg-yellow-100 border border-yellow-300 rounded-lg p-4 animate-fade-in">
-            <strong>Found Nothing! Please check spelling or Try again</strong>
+        {loading && <Loader />}
+        {searchResults.length > 0 ? (
+          <div className="mt-10">
+            <p className="text-left"><strong>Search Results:</strong></p>
+            <div className="max-h-40 overflow-y-auto mt-2 no-scrollbar">
+              {searchResults.map((pkg: any) => (
+                <div
+                  key={pkg.package.name}
+                  className={`text-left package-item p-2 cursor-pointer hover:bg-gray-300 ${
+                    pkg === selectedPackage ? "bg-blue-200" : ""
+                  }`}
+                  onClick={() => setSelectedPackage(pkg)}
+                >
+                  {pkg.package.name}
+                </div>
+              ))}
+            </div>
+            {searchResults.length > 4 && (
+              <p className="m-0 p-0 text-center">
+                <strong>. . .</strong>
+              </p>
+            )}
           </div>
-        )
-      )}
+        ) : (
+          debouncedValue &&
+          !loading && (
+            <div className="mt-10 text-center transform scale-100 translate-y-0 transition-opacity ease-in duration-500 bg-yellow-100 border border-yellow-300 rounded-lg p-4 animate-fade-in">
+              <strong>Found Nothing! Please check spelling or Try again</strong>
+            </div>
+          )
+        )}
 
-      {selectedPackage && (
-        <div className="mt-10">
-          <textarea
-            placeholder="Why is this your favorite package?"
-            value={favReason}
-            onChange={(e) => setFavReason(e.target.value)}
-            className="w-full p-2 border rounded bg-slate-200 "
-          ></textarea>
-        </div>
-      )}
-      {favReason.trim() !== "" && (
-        <div className="gap-3 flex flex-row justify-evenly items-center mt-5">
-          <PrimaryButton onClick={handleAddFav}>Add</PrimaryButton>
-          <DangerButton onClick={onCancel}>Cancel</DangerButton>
-        </div>
-      )}
+        {selectedPackage && (
+          <div className="mt-10">
+            <p className="text-left mb-1"><strong>Why is this your favorite?</strong></p>
+            <textarea
+              value={favReason}
+              onChange={(e) => setFavReason(e.target.value)}
+              className="w-full p-2 border rounded bg-slate-200 "
+            ></textarea>
+          </div>
+        )}
+        {favReason.trim() !== "" && (
+          <div className="mt-5 text-right">
+            <PrimaryButton onClick={handleAddFav}>Add</PrimaryButton>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
